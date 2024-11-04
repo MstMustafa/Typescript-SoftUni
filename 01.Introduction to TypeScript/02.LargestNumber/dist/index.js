42fffebaafaa2c10d9c6c03199403e939312cd86
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-11-04 18:46:13 +0200

    Add files via upload

---
+++ 01.Introduction to TypeScript/02.LargestNumber/dist/index.js
@@ -0,0 +1,5 @@
+function findLargestNumber(num1, num2, num3) {
+    return Math.max(num1, num2, num3);
+}
+var largest = findLargestNumber(5, -3, 16);
+console.log("The largest number is ".concat(largest, "."));
