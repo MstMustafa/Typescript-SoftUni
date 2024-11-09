34752086886d3d12c55c01d5640cd5a3c1e3dd7e
Mustafa Mustafa <141492875+MstMustafa@users.noreply.github.com>
2024-11-09 16:30:29 +0200

    Add files via upload

---
+++ 01.Introduction to TypeScript/05.Math Operations/dist/index.js
@@ -0,0 +1,25 @@
+function operation(num1, num2, operator) {
+    var result;
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        case '%':
+            result = num1 % num2;
+            break;
+        case '**':
+            result = Math.pow(num1, num2);
+            break;
+    }
+    console.log(result);
+}
+operation(5, 6, '+');
